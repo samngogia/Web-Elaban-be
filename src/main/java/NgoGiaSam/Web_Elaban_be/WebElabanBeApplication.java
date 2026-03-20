0308292dae34272430d6726776c02e5ef6bcd29e
samngogia <ngogiasamc3@gmail.com>
2026-03-20 15:44:58 +0700

    modau

---
+++ src/main/java/NgoGiaSam/Web_Elaban_be/WebElabanBeApplication.java
@@ -0,0 +1,13 @@
+package NgoGiaSam.Web_Elaban_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebElabanBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebElabanBeApplication.class, args);
+	}
+
+}
